d16f3f39e2758e0c9232e34da458001fcbaeba78
phannerl <36044017+phannerl@users.noreply.github.com>
2024-05-03 13:12:48 +0700

    update: Add clsx dependency and update package.json and package-lock.json

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>;
+import { useState } from 'react';
+
+export default function DashboardPage() {
+    const [click, setClick] = useState(false);
+
+    return <DashboardView click={click} setClick={setClick}/>;
+}
+
+import stypes from '@/ui/dashboard.module.css';
+import clsx from 'clsx';
+
+interface DashboardViewProps {
+    click: boolean;
+    setClick: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function DashboardView({click, setClick}: Readonly<DashboardViewProps>) {
+
+    return (
+        <div className="flex h-screen justify-center items-center flex-col">
+            <h1 className={clsx({"bg-gradient-to-r from-violet-500 to-fuchsia-500": click === true})}>Hello, Dashboard Page!</h1>
+            <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+            <br />
+            <div className={stypes.box}>
+                Box
+            </div>
+            <div>
+                <button onClick={() => setClick(!click)}>{click?"Clicked":"Click"}</button>
+            </div>
+        </div>
+    );
 }
