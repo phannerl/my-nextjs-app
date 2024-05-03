38c4f14da21ba2e516e062a71dfa221e6debaa98
phannerl <36044017+phannerl@users.noreply.github.com>
2024-05-03 14:18:36 +0700

    update: Update DashboardPage component to use User interface and fetch_data function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,16 @@ import stypes from "@/ui/dashboard.module.css";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    phone: string;
+    address: string;
+}
 export default function DashboardPage() {
     const [click, setClick] = useState(false);
-    const [fetchedData, setFetchedData] = useState([]);
+    const [fetchedData, setFetchedData] = useState<User[]>([]);
     const handleClicked = () => {
         console.log("Clicked", fetchedData);
         setClick(!click);
